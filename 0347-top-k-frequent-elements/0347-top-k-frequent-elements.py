25115cfa97d83d810d05278898722a49feefb2af
C S Narendra Vignesh <csnarendravignesh@gmail.com>
2026-09-19 12:09:34 +0530

    Time: 15 ms (32.76%), Space: 20.1 MB (10.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,11 +7,19 @@ class Solution(object):
         """
         count={}
         for i in nums:
-            if i in count:
+    
+            if  i in count:
                 count[i]+=1
             else:
                 count[i]=1
-        sorted_Result=sorted(count,key=count.get,reverse=True)
-        return sorted_Result[:k]
-
+        buckets=[[] for j in range(len(nums)+1)]
         
+        for i,freq in count.items():
+            buckets[freq].append(i)
+        result=[]
+        for freq in range(len(buckets)-1,0,-1):
+            for i in buckets[freq]:
+                result.append(i)
+                if len(result)==k:
+                    return result
+        return result
