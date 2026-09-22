7d1544472c1a901a4441cc35371082136ec83fae
C S Narendra Vignesh <csnarendravignesh@gmail.com>
2026-09-22 23:33:59 +0530

    Time: 123 ms (11.07%), Space: 15 MB (63.87%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, nums):
+        total = sum(nums)
+
+        # Normal maximum subarray
+        current_max = nums[0]
+        max_sum = nums[0]
+
+        # Minimum subarray
+        current_min = nums[0]
+        min_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            current_max = max(nums[i], current_max + nums[i])
+            max_sum = max(max_sum, current_max)
+
+            current_min = min(nums[i], current_min + nums[i])
+            min_sum = min(min_sum, current_min)
+
+        # If all numbers are negative
+        if max_sum < 0:
+            return max_sum
+
+        # Circular maximum
+        circular_sum = total - min_sum
+
+        return max(max_sum, circular_sum)
+
+
+# Sample dataset
+nums = [5, -3, 5]
+
+solution = Solution()
+
+print(solution.maxSubarraySumCircular(nums))
