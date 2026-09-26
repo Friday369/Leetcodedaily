5998e1a2b0a538292719ffffae07e8e748185d6d
C S Narendra Vignesh <csnarendravignesh@gmail.com>
2026-09-26 18:47:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (22.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,29 +1,24 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        arr1 = nums1[:m]
-        result = []
-
-        i = 0
-        j = 0
-
-        while i < m and j < n:
-            if arr1[i] <= nums2[j]:
-                result.append(arr1[i])
-                i += 1
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i=m-1
+        j=n-1
+        k=m+n-1
+        while i>=0 and j>=0:
+            if nums1[i]>=nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
             else:
-                result.append(nums2[j])
-                j += 1
-
-        # Remaining elements from arr1
-        while i < m:
-            result.append(arr1[i])
-            i += 1
-
-        # Remaining elements from nums2
-        while j < n:
-            result.append(nums2[j])
-            j += 1
-
-        # Put result into nums1
-        for i in range(m + n):
-            nums1[i] = result[i]
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+        while j>=0:
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
